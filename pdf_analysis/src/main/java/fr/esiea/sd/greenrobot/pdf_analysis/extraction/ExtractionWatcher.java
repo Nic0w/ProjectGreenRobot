5319c702c90d7fa0ce1b561d7e9f32b1e5f12e4a
Nic0w <nico.broquet@gmail.com>
2013-02-06 20:28:56 +0100

    Forgot these two classes.

---
+++ pdf_analysis/src/main/java/fr/esiea/sd/greenrobot/pdf_analysis/extraction/ExtractionWatcher.java
@@ -0,0 +1,5 @@
+package fr.esiea.sd.greenrobot.pdf_analysis.extraction;
+
+public interface ExtractionWatcher {
+
+}
---
+++ pdf_analysis/src/main/java/fr/esiea/sd/greenrobot/pdf_analysis/extraction/PagesCounter.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.esiea.sd.greenrobot.pdf_analysis.extraction;
+
+/**
+ * @author nic0w
+ *
+ */
+public interface PagesCounter {
+
+	public void addOnePage();
+	
+}
