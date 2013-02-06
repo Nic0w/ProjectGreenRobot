04af05f6fa9270c775ded7428ce3a1ca219c9c99
Nic0w <nico.broquet@gmail.com>
2013-02-06 16:15:54 +0100

    Added Apache Commons FileUpload in dependencies.

--- website/src/main/java/fr/esiea/sd/greenrobot/website/PDFReceiver.java
+++ website/src/main/java/fr/esiea/sd/greenrobot/website/PDFReceiver.java
@@ -1,11 +1,19 @@
 package fr.esiea.sd.greenrobot.website;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet implementation class PDFReceiver
@@ -35,7 +43,30 @@ public class PDFReceiver extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//POST Method is used when the user upload a file.
-		response.getWriter().printf("Downloading a PDF from somewhere !");
+		//response.getWriter().printf("User is uploading a file...");
+		
+		try {
+	        List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+	        for (FileItem item : items) {
+	            if (item.isFormField()) {
+	                // Process regular form field (input type="text|radio|checkbox|etc", select, etc).
+	                String fieldname = item.getFieldName();
+	                String fieldvalue = item.getString();
+	                // ... (do your job here)
+	            } else {
+	            	
+	                // Process form file field (input type="file").
+	                String fieldname = item.getFieldName();
+	                String filename = item.getName();
+	                //InputStream filecontent = item.getInputStream();
+	                response.getWriter().printf("User is uploading a file of " + item.getSize() + " bytes named " + filename);
+	            }
+	        }
+	    } catch (FileUploadException e) {
+	        throw new ServletException("Cannot parse multipart request.", e);
+	    }
+		
+		
 	}
 
 }
