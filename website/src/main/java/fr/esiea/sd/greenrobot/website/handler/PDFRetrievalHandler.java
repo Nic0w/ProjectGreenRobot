3176a1b3e64a6f784ac85788aca5c94affaef480
Nic0w <nico.broquet@gmail.com>
2013-02-10 00:31:12 +0100

    Removed request parsing from async call() function.

--- website/src/main/java/fr/esiea/sd/greenrobot/website/handler/PDFRetrievalHandler.java
+++ website/src/main/java/fr/esiea/sd/greenrobot/website/handler/PDFRetrievalHandler.java
@@ -11,6 +11,7 @@ import java.util.concurrent.Callable;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FileUtils;
@@ -18,6 +19,7 @@ import org.apache.commons.io.FileUtils;
 import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.SettableFuture;
 
 /**
  * @author nic0w
@@ -26,7 +28,7 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 public class PDFRetrievalHandler {
 
 	private final ServletFileUpload fileUploadManager;
-	
+
 	/**
 	 * 
 	 */
@@ -36,47 +38,55 @@ public class PDFRetrievalHandler {
 
 	public ListenableFuture<File> retrieveFile(final HttpServletRequest request, ListeningExecutorService executor) {
 		Preconditions.checkNotNull(request, "Request is null.");
-		Preconditions.checkNotNull(request.getContentType(), "Content-Type is null.");
 		Preconditions.checkNotNull(executor, "Executor is null.");
-		
-		return executor.submit(new Callable<File>() {
-			@Override
-			public File call() throws Exception {
-				Preconditions.checkNotNull(request.getContentType(), "Content-Type is null.");
-				
-				List<FileItem> formItems = fileUploadManager.parseRequest(request);
-				
-				if(formItems.size()>0) {
-				
-					FileItem firstItem = formItems.get(0); //We are only interested in the first item.
-					
-					File tempFile = File.createTempFile("greenrobot", null);
-					
-					if(firstItem.isFormField()) { //Then this is an URL to a PDF File we have to download.
-					
-						String fieldName = firstItem.getFieldName();
-						
-						if(!fieldName.contentEquals("url"))
-							throw new IllegalArgumentException("The field 'url' was not found in the form !");
-						
-						String fieldValue = firstItem.getString();
-						
-						URL fileURL = new URL(fieldValue);
-						
-						//We are force to have Commons IO as a dependency because of PDFBox, so let's use it !
-						FileUtils.copyURLToFile(fileURL, tempFile, 5000, 5000);						
+
+		try {
+			final List<FileItem> formItems = this.fileUploadManager.parseRequest(request);
+
+			return executor.submit(new Callable<File>() {
+				@Override
+				public File call() throws Exception {
+
+					if(formItems.size()>0) {
+
+						FileItem firstItem = formItems.get(0); //We are only interested in the first item.
+
+						File tempFile = File.createTempFile("greenrobot", null);
+
+						if(firstItem.isFormField()) { //Then this is an URL to a PDF File we have to download.
+
+							String fieldName = firstItem.getFieldName();
+
+							if(!fieldName.contentEquals("url"))
+								throw new IllegalArgumentException("The field 'url' was not found in the form !");
+
+							String fieldValue = firstItem.getString();
+
+							URL fileURL = new URL(fieldValue);
+
+							//We are force to have Commons IO as a dependency because of PDFBox, so let's use it !
+							FileUtils.copyURLToFile(fileURL, tempFile, 5000, 5000);						
+						}
+						else { //The file is uploaded.
+
+							FileUtils.copyInputStreamToFile(firstItem.getInputStream(), tempFile);	
+						}
+
+						return tempFile;
 					}
-					else { //The file is uploaded.
-						
-						FileUtils.copyInputStreamToFile(firstItem.getInputStream(), tempFile);	
-					}
-					
-					return tempFile;
+					else
+						throw new IllegalArgumentException("The form submitted is empty !");
 				}
-				else
-					throw new IllegalArgumentException("The form submitted is empty !");
-			}
-		});
+			});
+
+		} catch (FileUploadException e) {
+
+			SettableFuture<File> failedFuture = SettableFuture.create();
+
+			failedFuture.setException(e);
+			return failedFuture;
+		}
+
 	}
 
 }
